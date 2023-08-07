9fe89ab8c078b4f4c2a8392558a58ad1ad68dd8f
Michael Luna <michaelluna244@gmail.com>
2023-08-07 15:42:24 -0500

    adding new file for git to track

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Kids Cash</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @livewireStyles
+
+
+    </head>
+    <body>
+        <h1><a href="/students">Dashboard</a></h1>
+    </body>
+</html>
