330f884535eff10d80d25e3e138f07cf1edd77ce
Michael Luna <michaelluna244@gmail.com>
2023-08-28 16:55:40 -0500

    still working on things

--- app/Http/Livewire/Points.php
+++ app/Http/Livewire/Points.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Point;
 use App\Models\Student;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Points extends Component
@@ -16,6 +17,7 @@ class Points extends Component
         $this->studentId = $studentId;
     }
 
+
     public function getTotalPoints()
     {
         return array_sum($this->selectedPoints);
@@ -23,17 +25,34 @@ class Points extends Component
 
     public function submitPoints()
     {
+        // Check if any points are selected
+        if (count($this->selectedPoints) === 0) {
+            // Set an error message here if needed
+            return;
+        }
+//        if ($this->selectedPoints->contains(false)){
+//            $element = $this->selectedPoints->contains(false);
+//            $this->selectedPoints->forget(false);
+//            ddd($this->selectedPoints);
+//        }
+
+
         $totalPoints = $this->getTotalPoints();
         $student = Student::findOrFail($this->studentId);
-        $student->points += $totalPoints;
-        $student->save();
 
-        $this->reset(); // Clear selected points
+        // Perform the transaction logic here
+        try {
+            $student->points += $totalPoints;
+            $student->save();
+            $this->selectedPoints = []; // Clear selected points
 
-        // Redirect back to the student view
-        return redirect('/rooms/' . strtolower($student->room->name));
+            // Set success message
+            return redirect('/rooms/' . strtolower($student->room->name))->with('success_message', "Point transaction of $totalPoints completed");
+        } catch (\Exception $e) {
+            // Set error message
+            session()->flash('error_message', 'An error occurred during the transaction');
+        }
     }
-
     public function render()
     {
         $points = Point::all();
--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
     <h1>{{$room->name}} Room</h1>
+
+    {{-- Message --}}
+    @if (Session::has('success_message'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Success!</strong> {{ session('success_message') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
     <table class="table table-hover table-bordered">
         <thead>
         <tr>
@@ -21,7 +30,7 @@
                     </td>
                 </tr>
         @endforeach
-
         </tbody>
     </table>
 </x-layout>
+
