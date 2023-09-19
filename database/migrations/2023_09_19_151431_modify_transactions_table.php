3df536d217e740f49c3b15a5f32482b906d19a56
Michael Luna <michaelluna244@gmail.com>
2023-09-19 16:36:47 -0500

    Adding the modify transaction file

---
+++ database/migrations/2023_09_19_151431_modify_transactions_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            // Modify the 'type' column to a new data type
+            $table->enum('type', ['Earned', 'Spent', 'Refund'])->default('Earned')->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            // Reverse the changes made in the 'up' method
+            $table->integer('type')->change();
+        });
+    }
+};
